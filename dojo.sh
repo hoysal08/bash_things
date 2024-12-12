4d48b7639fd281cbd73febb078e7053db25de49e
sooraj hoysal <sooraj.a@gdn-commerce.com>
2024-12-12 10:38:16 +0530

    basic bashy

---
+++ dojo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#iAmVar=morning
+echo "Hello world, from bash"
+#echo $iAmV
+read name
+echo "Hi there $name"
+read -p "Whats your name " name2
+echo "Hi, there $name2"
+
+
---
+++ suicide.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "The name of the file is: $0 and it is going to be selfdeleted."
+rm -f $0
